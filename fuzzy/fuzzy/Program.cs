c25b2e23860287188e98964cb3ff99dc4c18e577
Brian Wagener <brianw@avalamarketing.com>
2014-08-12 12:54:36 -0500

    Initial commit.

---
+++ fuzzy/fuzzy/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fuzzy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch timer = new Stopwatch();
+            string query;
+            int maxDistance;
+
+            if (args.Length != 2 || !int.TryParse(args[1], out maxDistance))
+            {
+                Console.WriteLine("Usage: fuzzy <query> <distance>");
+                return;
+            }
+            else
+            {
+                query = args[0];
+            }
+
+            timer.Start();
+            TrieNode trie = new TrieNode();
+
+            foreach (string name in Names()) trie.Insert(name);
+            timer.Stop();
+
+            Console.WriteLine("Loaded {0} words in {1} nodes in {2} seconds.", TrieNode.WordCount, TrieNode.NodeCount, timer.Elapsed.TotalSeconds);
+            Console.WriteLine();
+            Console.WriteLine("Searching for '{0}'...", query);
+            Console.WriteLine();
+            Console.WriteLine("Results:");
+            timer.Reset();
+
+            timer.Start();
+            int resultCount = 0;
+            foreach (string result in trie.Search(query, maxDistance))
+            {
+                resultCount++;
+                Console.Write("{0} ", result);
+            }
+
+            timer.Stop();
+
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("Found {0} results for '{1}' with a max distance of {2} in {3} seconds.", 
+                resultCount, query, maxDistance, timer.Elapsed.TotalSeconds);
+
+        }
+
+
+        static IEnumerable<string> Names()
+        {
+            using (StreamReader sw = new StreamReader("names.txt"))
+            {
+                while (!sw.EndOfStream)
+                {
+                    yield return sw.ReadLine();
+                }
+                sw.Close();
+            }
+        }
+    }
+}
---
+++ fuzzy/fuzzy/TrieNode.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fuzzy
+{
+    public class TrieNode
+    {
+        public static int NodeCount = 0;
+        public static int WordCount = 0;
+
+        public string Word { get; set; }
+        public Dictionary<char, TrieNode> Children { get; private set; }
+
+        public TrieNode()
+        {
+            TrieNode.NodeCount++;
+            Children = new Dictionary<char, TrieNode>();
+        }
+
+        public void Insert(string word)
+        {
+            TrieNode node = this;
+            for (int i = 0, j = word.Length; i < j; i++)
+            {
+                if (!node.Children.ContainsKey(word[i]))
+                {
+                    node.Children.Add(word[i], new TrieNode());
+                }
+
+                node = node.Children[word[i]];
+            }
+
+            node.Word = word;
+            TrieNode.WordCount++;
+        }
+
+        public IEnumerable<string> Search(string word, int maxDistance)
+        {
+            int[] currentRow = Enumerable.Range(0, word.Length + 1).ToArray();
+
+            foreach (char key in Children.Keys)
+            {
+                foreach (string result in SearchRecursive(Children[key], key, word, currentRow, maxDistance))
+                    yield return result;
+            }
+
+            yield return string.Empty;
+        }
+
+        private IEnumerable<string> SearchRecursive(TrieNode node, char letter, string word, int[] previousRow, int maxDistance)
+        {
+            int columns = word.Length + 1;
+            int[] currentRow = new int[columns];
+
+            currentRow[0] = previousRow[0] + 1;
+
+            for (int i = 1, j = columns; i < j; i++)
+            {
+                int insertCost = currentRow[i -1] + 1;
+                int deleteCost = previousRow[i] + 1;
+
+                int replaceCost = 0;
+                if (word[i -1] != letter) 
+                    replaceCost = previousRow[i -1] + 1;
+                else
+                    replaceCost = previousRow[i -1];
+
+                currentRow[i] = Math.Min(Math.Min(insertCost, deleteCost), replaceCost);
+            }
+
+            if (currentRow[columns -1] <= maxDistance && node.Word != null)
+                yield return node.Word;
+
+            if (currentRow.Min() <= maxDistance)
+            {
+                foreach (char key in node.Children.Keys)
+                {
+                    foreach (string result in SearchRecursive(node.Children[key], key, word, currentRow, maxDistance))
+                        yield return result;
+                }
+            }
+        }
+    }
+}
